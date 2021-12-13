6d07cdfca8890ea6316be2fe990addba27ea78cc
Justin Hall <46795053+hall4jm@users.noreply.github.com>
2021-12-12 18:50:13 -0600

    Add code and directories

---
+++ scripts/train.py
@@ -0,0 +1,182 @@
+from os import environ
+import sys
+import getopt
+import json
+import fastai
+import matplotlib.pyplot as plt
+
+from fastai.vision.all import *
+from fastai.vision.models import xresnet
+import optuna
+from optuna.integration import FastAIPruningCallback
+import neptune.new as neptune
+import neptune.new.integrations.optuna as optuna_utils
+
+def read_json(file_path: str):
+    """
+    Reads in config file (json) format
+    :param (str) path to config
+    :ret (dict) the config file
+    """
+    with open(file_path, 'r') as json_obj:
+        this_config = json.load(json_obj)  # read in json object
+
+    return this_config
+
+def get_val(dict, key):
+    """
+    Gets value from dictionary
+    """
+    return dict[key] if key in dict else None
+
+def get_tune_hp(trial, dict):
+    return getattr(trial,dict["dist"])(*dict["vals"].values())
+
+
+def objective(trial, environ, hps, tune, arch):
+
+    lr = get_val(tune, 'lr')
+    wd = get_val(tune, 'weight_decay')
+    moms = get_val(tune, 'momentum')
+    batch_size = get_val(tune, 'batch_size')
+    freeze_epochs = get_val(tune, 'freeze_epochs')
+
+    start_mom = get_tune_hp(trial, moms)
+    min_mom = trial.suggest_float('min_mom', .8, start_mom)
+    end_mom = trial.suggest_float('end_mom', min_mom, .999)
+
+    tfms = aug_transforms(
+        max_rotate=get_val(hps, 'max_rotate'),
+        max_zoom=get_val(hps, 'max_zoom'),
+        p_affine=get_val(hps, 'p_affine'),
+        size = get_val(hps, 'batch_image_resize'),
+        min_scale=get_val(hps, 'min_scale')
+    )
+
+    breast_us = DataBlock(
+        blocks=(ImageBlock, CategoryBlock), 
+        get_items=get_image_files, 
+        splitter=GrandparentSplitter('train', 'val'),
+        #splitter=RandomSplitter(valid_pct=.2, seed = 42),
+        get_y=parent_label,
+        batch_tfms = tfms,
+        item_tfms=Resize(get_val(hps, 'image_size'))
+    )
+
+    dls = breast_us.dataloaders(get_val(environ, 'data_dir'), bs = get_tune_hp(trial, batch_size)).cuda()
+
+    #Define additional metrics for comparison
+    prec = Precision(average='binary')
+    recall = Recall(average='binary')
+    auc = RocAucBinary()
+    f1 = F1Score(average='binary')
+
+    #define model
+    model = getattr(fastai.vision.models.all, get_val(arch, 'backbone'))
+
+    learn = cnn_learner(
+      dls,
+      model,
+      opt_func = getattr(fastai.optimizer,get_val(hps, 'opt')),
+      lr = get_tune_hp(trial, lr),
+      wd = get_tune_hp(trial, wd),
+      moms = (start_mom, min_mom, end_mom),
+      metrics = [auc, prec, recall, accuracy, f1],
+      loss_func = getattr(fastai.losses, get_val(hps, 'loss_func'))(),
+      cbs = [FastAIPruningCallback(trial, monitor = "roc_auc_score"), MixUp()]
+    )
+
+    # to disable progress bar and logging info
+    with learn.no_bar():
+      with learn.no_logging():
+        learn.fine_tune(get_val(hps, 'num_epochs'), freeze_epochs = get_tune_hp(trial, freeze_epochs))
+
+        #Save model
+        model_name = get_val(environ, "model_dir") + "/" + get_val(arch, 'backbone') + '_trial_' + str(trial.number) + ".pkl"
+        learn.export(model_name)
+
+        learn.recorder.plot_loss()
+        plt.savefig(get_val(environ, "img_dir") + "/" + get_val(arch, 'backbone') + '_loss_trial_' + str(trial.number) + ".png")
+        plt.close()
+        
+        learn.recorder.plot_sched()
+        plt.savefig(get_val(environ, "img_dir") + "/" + get_val(arch, 'backbone') + '_sched_trial_' + str(trial.number) + ".png")
+        plt.close()
+
+        interp = ClassificationInterpretation.from_learner(learn)
+        interp.plot_confusion_matrix()
+        plt.savefig(get_val(environ, "img_dir") + "/" + get_val(arch, 'backbone') + '_conf_matrix_trial_' + str(trial.number) + ".png")
+        plt.close()
+
+        interp.plot_top_losses(16, figsize = (14,14))
+        plt.savefig(get_val(environ, "img_dir") + "/" + get_val(arch, 'backbone') + '_top_losses_trial_' + str(trial.number) + ".png")
+        plt.close()
+    return learn.recorder.metrics[0].value.item()
+
+def main(argv):
+    
+    #Default values
+    config_file = None
+    
+    try:
+        opts, args = getopt.getopt(argv,"c:")
+    except getopt.GetoptError:
+        print('train.py -c <config_json>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-c"):
+            config_file= arg
+
+    config = read_json(config_file)
+    environ = get_val(config, 'environ')
+    hps = get_val(config, 'hps')
+    tune = get_val(config, 'tune') 
+    arch = get_val(hps, 'arch')
+
+    pruner = optuna.pruners.MedianPruner(n_startup_trials = 5, n_min_trials = 15)
+    study = optuna.create_study(study_name = get_val(environ, "study_name"), direction="maximize", pruner=pruner)
+
+    study.optimize(lambda trial: objective(trial, environ, hps, tune, arch), n_trials=get_val(environ, "n_trials"))
+
+    best_learn = load_learner(get_val(environ, "model_dir") + "/" + get_val(arch, 'backbone') + '_trial_' + str(study.best_trial.number) + ".pkl")
+    best_conf = get_val(environ, "img_dir") + "/" + get_val(arch, 'backbone') + '_conf_matrix_trial_' + str(study.best_trial.number) + ".png"
+    best_loss = get_val(environ, "img_dir") + "/" + get_val(arch, 'backbone') + '_loss_trial_' + str(study.best_trial.number) + ".png"
+    best_top = get_val(environ, "img_dir") + "/" + get_val(arch, 'backbone') + '_top_losses_trial_' + str(study.best_trial.number) + ".png"
+    best_sched = get_val(environ, "img_dir") + "/" + get_val(arch, 'backbone') + '_sched_trial_' + str(study.best_trial.number) + ".png"
+    
+    run = neptune.init(
+        project="<PROJECT_NAME>",
+        api_token="<API_TOKEN>",
+        tags = [get_val(arch, 'backbone')]
+    )
+
+    optuna_utils.log_study_metadata(study, run)
+
+    run["best/precision"] = best_learn.metrics.items[1].value.item()
+    run["best/recall"] = best_learn.metrics.items[2].value.item()
+    run["best/accuracy"] = best_learn.metrics.items[3].value.item()
+    run["best/f1"] = best_learn.metrics.items[4].value.item()
+
+    run["study/num_trials"]  = get_val(environ, "n_trials")
+
+    run["train/params/Epochs"] = get_val(hps, 'num_epochs')
+    run["train/params/Img Size"] = (get_val(hps, 'batch_image_resize'), get_val(hps, 'batch_image_resize'))
+    run["train/params/loss_func"] = get_val(hps, 'loss_func')
+    run["train/params/opt"] = get_val(hps, 'opt')
+    run["train/params/scheduler"] = get_val(hps, 'scheduler')
+
+    run["augments/max_rotate"] = get_val(hps, 'max_rotate')
+    run["augments/max_zoom"] = get_val(hps, 'max_zoom')
+    run["augments/p_affine"] = get_val(hps, 'p_affine')
+    run["augments/batch_image_resize"] = (get_val(hps, 'image_size'), get_val(hps, 'image_size'))
+
+    run['visualizations/conf_matrix'].upload(best_conf)
+    run['visualizations/train_loss'].upload(best_loss)
+    run['visualizations/top_losses'].upload(best_top)
+    run['visualizations/sched'].upload(best_sched)
+
+    run.stop()
+    
+if __name__ == "__main__":
+  main(sys.argv[1:])
+
